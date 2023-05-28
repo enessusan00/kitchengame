7dbdac4a0fe44a18df543e1730037cf4bd5e25d6
enessusan00 <191118029@samsun.edu.tr>
2023-05-28 11:14:43 +0300

    collision detection

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+    
+    private void Awake() {
+     playerInputActions= new PlayerInputActions();
+     playerInputActions.Player.Enable();
+}
     public Vector2 GetMovementVectorNormalized(){
-         Vector2  inputVector  = new Vector2(0,0);// klavyede sadece iki boyut vardır bu yüzden başta 2 olarak tanımlanır
-        if(Input.GetKey(KeyCode.W)){
-            inputVector.y +=1;
-        }else if(Input.GetKey(KeyCode.A)){
-            inputVector.x -=1;
-        }else if(Input.GetKey(KeyCode.S)){
-            inputVector.y -=1;
-        }else if(Input.GetKey(KeyCode.D)){
-            inputVector.x +=1;
-        }
+         Vector2  inputVector  =playerInputActions.Player.Move.ReadValue<Vector2>();// klavyede sadece iki boyut vardır bu yüzden başta 2 olarak tanımlanır
+
          inputVector =inputVector.normalized;
          return inputVector;
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,32 @@ public class Player : MonoBehaviour
          Vector3 movDir =new Vector3(inputVector.x,0f,inputVector.y); // karakteri zeminde yürümesini sağlar 
        
         isWalking = movDir !=Vector3.zero;
-        transform.position+=movDir*moveSpeed*Time.deltaTime;
+        float playerRadius=.7f;
+        float movDistance= moveSpeed*Time.deltaTime;
+        float playerHeight = .7f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight ,playerRadius,movDir,movDistance);
+        if(!canMove){
+            // eğer hareket yönünün tersine gidemiyorsak
+            //sadece x yönüne izin ver
+            Vector3 moveDirX= new Vector3(movDir.x,0,0);
+            canMove =!Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight ,playerRadius,moveDirX,movDistance);
+            if(canMove){
+                //eğer sadece x de hareket edebiliyorsak
+                movDir=moveDirX;
+            }else {
+                // eğer sadece x yönünde hareket edemiyorsak  z ye izin ver
+                Vector3 moveDirZ = new Vector3(0,0,movDir.z);
+                canMove =!Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight ,playerRadius,moveDirZ,movDistance);
+                if(canMove){
+                    //z yönüne izin ver
+                    movDir=moveDirZ;
+                }
+                else{
+                    //hiçbir yönde hareket edemiyor
+                }
+            }
+        transform.position+=movDir*movDistance;
+        }
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,movDir,Time.deltaTime*rotateSpeed);
  
